54df38c0240962f420db07af523ecdbb73aa208e
Yurii Serhiichuk <savik.ne@gmail.com>
2022-06-24 23:01:18 +0300

    Define Python generator

---
+++ generators/python/generate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+# This script runs Swagger API codegen CLI and generated Python SDK using provided config.
+#
+# It is expected that `urbandictionary-api.yaml`is available in the script folder.
+#
+
+SPEC_FILE="urbandictionary-api.yaml"
+CONFIG_FILE="python.json"
+
+cp "../../${urbandictionary-api.yaml}" "./${urbandictionary-api.yaml}"
+
+docker run --rm -it -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate \
+  -i /local/${SPEC_FILE} \
+  -l python \
+  -o /local/sdk \
+  -c /local/"${CONFIG_FILE}"
